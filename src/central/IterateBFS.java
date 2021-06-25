0cfbc21199f5febcb915c30ddb38be0c335ce119
Jonathan A. Saddler <1129384+jazad136@users.noreply.github.com>
2021-06-25 11:38:34 -0400

    Initial Source Code with License

--- src/central/IterateBFS.java
+++ src/central/IterateBFS.java
@@ -1,9 +1,30 @@
+/*******************************************************************************
+ *    Copyright (c) 2018 Jonathan A. Saddler
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *    
+ *    Contributors:
+ *     Jonathan A. Saddler 
+ *******************************************************************************/
 package central;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.NodeList;
 
 public class IterateBFS {
 	
@@ -12,20 +33,29 @@ public class IterateBFS {
 	public Document xmlDoc;
 	static Definition lastSibling = Definition.emptyDef;
 	
-	public IterateBFS() { } 
-	
-	
-	public void startXMLBFS(Document xmlDoc) {  }
-	
-	protected class BFSVisitor { 
-		Queue Q;
-		int parent; 
-		public BFSVisitor(Document xmlDoc) {  }
+	public IterateBFS(Document xmlDoc) { 
+		this.xmlDoc = xmlDoc;
 	}
+	
 	
 	public static void errorOut(Exception e) { 
 		System.err.println(e.getClass().getSimpleName() + ":");
 		System.err.println(e.getLocalizedMessage());
 		System.err.println("at\t" + ErrorOut.someStackTrace(4, e));
 	}
+	public void startXMLBFS() { 
+	}
+	public void continueXMLBFS() { 
+		
+	}
+	protected class BFSVisitor { 
+		Queue Q;
+		int parent; 
+		public BFSVisitor(Document xmlDoc) { 
+		}
+		
+		public boolean bfsDone() { return Q.isEmpty(); } 
+		
+	}
+	
 }
